7443b4f5b2dec6c27e88327734b594227cdc02ba
Throuz <zed2786328@gmail.com>
2025-08-13 13:23:36 +0800

    Update title in App component to reflect Vite CSR

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + CSR</h1>
+      <h1>Vite CSR</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
